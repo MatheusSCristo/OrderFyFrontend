e5497be2b922746e6fe7953b41f41111273c0ce7
MatheusSCristo <cristo.matheus@outlook.com>
2025-08-14 21:18:37 -0300

    feat: add axios config

---
+++ src/api/axiosConfig.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status == 401) {
+      Cookies.remove("token");
+      setTimeout(() => {
+        window.location.href = "/";
+      }, 2000);
+    }
+
+    return Promise.reject(error);
+  }
+);
